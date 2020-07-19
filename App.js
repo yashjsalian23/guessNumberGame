f913f13af9f855efc1baef7030da62772a1011d6
yashjsalian23 <yashjsalian23@gmail.com>
2020-07-19 17:01:47 +0530

    added custom fonts

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 
 import Header from './components/Header';
 import StartGameScreen from './screen/StartGameScreen';
 import GameScreen from './screen/GameScreen';
 import GameOverScreen from './screen/GameOverScreen';
 
+const fetchFonts = () => {
+  Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+}
+
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if(!dataLoaded){
+    return (
+      <AppLoading startAsync={fetchFonts}
+      onFinish={()=>setDataLoaded(true)}
+      onError={()=>console.log(err)} />
+    );
+  }
 
   const configureNewGameHandler = () => {
     setGuessRounds(0);
